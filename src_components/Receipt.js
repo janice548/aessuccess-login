6f9d6960367550613371eb9555aa93b021922712
janice548 <janice548conner@outlook.com>
2025-09-13 14:05:17 +0530

    Create Receipt.js

---
+++ src_components/Receipt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt-container">
+      <p>Receipt Upload (Future Feature)</p>
+    </div>
+  );
+}
+
+export default Receipt;
